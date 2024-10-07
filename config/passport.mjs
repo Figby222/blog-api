d471d12025a9e8d67942280e52ffb55656161402
Figby222 <figby222@outlook.com>
2024-10-06 20:11:27 -0700

    Add passport.mjs

---
+++ config/passport.mjs
@@ -0,0 +1,34 @@
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import pool from "../db/pool.mjs";
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await pool.user.findUnique({
+                where: {
+                    username: username
+                }
+            });
+    
+            if (!user) {
+                done(null, false, { message: "Incorrect username or password" });
+                return;
+            }
+    
+            const isMatch = await bcrypt.compare(password, user.password);
+    
+            if (!isMatch) {
+                done(null, false, { message :"Incorrect username or password" });
+            }
+    
+            return done(null, user);
+        }
+        catch(err) {
+            done(err);
+        }
+    })
+)
+
+
+export default passport;
