28106c56145d490c31b765680c76c96ecb06d84a
Figby222 <figby222@outlook.com>
2024-10-06 19:17:22 -0700

    Add populatedb.mjs

---
+++ db/populatedb.mjs
@@ -0,0 +1,37 @@
+import pool from "./pool.mjs";
+
+async function main() {
+    await pool.user.createManyAndReturn({
+        data: [
+            { username: "cookies", email: "cookies@cookies.com", password: "cookies" },
+            { username: "sandwich", email: "sandwich@sandwich.com", password: "sandwich" },
+            { username: "cookiesSandwich", email: "cookies@sandwich.com", password: "cookiesSandwich" },
+            { username: "sandwichCookies", email: "sandwich@cookies.com", password: "sandwichCookies" }
+        ]
+    }).then(console.log);
+
+    await pool.post.createManyAndReturn({
+        data: [
+            { title: "Hi", text: "Stuff", creatorId: 1 },
+            { title: "Hi again", text: "Stuff again", creatorId: 2 },
+            { title: "Super Hi again again", text: "Super Stuff again again", creatorId: 3 },
+            { title: "Super Hi again again again", text: "Super Stuff again again again", creatorId: 4 }
+
+        ]
+    }).then(console.log);
+
+    await pool.comment.createManyAndReturn({
+        data: [
+            { text: "Hi", creatorId: 1, postId: 1 },
+            { text: "Hi Hi", creatorId: 2, postId: 2 },
+            { text: "Hi Hi Hi", creatorId: 3, postId: 3 },
+            { text: "Hi Hi Hi Hi", creatorId: 4, postId: 4 }
+        ]
+    }).then(console.log);
+}
+
+main()
+    .catch(async (err) => {
+        console.log(err);
+        await pool.$disconnect();
+    })
