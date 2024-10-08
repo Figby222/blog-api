b82e77788bff460a4fd91380ea56de8f8ef421ed
Figby222 <figby222@outlook.com>
2024-10-07 21:56:35 -0700

    Use usersRouter on /users path

--- routers/api/v1/v1Router.mjs
+++ routers/api/v1/v1Router.mjs
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import usersRouter from "./usersRouter.mjs";
 import * as indexController from "../../../controllers/api/v1/indexController.mjs";
 
 const indexRouter = Router();
+indexRouter.use("/users", usersRouter);
 
 indexRouter.get("/", indexController.indexRouteGet);
 
