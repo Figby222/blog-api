2d4f1e8c31b36ca511c8e04c7ff2e02c1bef4ae1
Figby222 <figby222@outlook.com>
2024-10-08 21:05:32 -0700

    Use postsRouter on /posts path

--- routers/api/v1/v1Router.mjs
+++ routers/api/v1/v1Router.mjs
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import postsRouter from "./postsRouter.mjs";
 import usersRouter from "./usersRouter.mjs";
 import * as indexController from "../../../controllers/api/v1/indexController.mjs";
 
 const indexRouter = Router();
 indexRouter.use("/users", usersRouter);
+indexRouter.use("/posts", postsRouter);
 
 indexRouter.get("/", indexController.indexRouteGet);
 
