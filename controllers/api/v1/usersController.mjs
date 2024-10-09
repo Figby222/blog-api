e0079694ed580b42d787307eb2371813ed219dda
Figby222 <figby222@outlook.com>
2024-10-08 19:34:38 -0700

    Change db path to userQueries.mjs

--- controllers/api/v1/usersController.mjs
+++ controllers/api/v1/usersController.mjs
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import db from "../../../db/queries/api/v1/usersQueries.mjs";
+import db from "../../../db/queries/api/v1/userQueries.mjs";
 
 const usersListGet = asyncHandler(async (req, res) => {
     const users = await db.getUsersList();
