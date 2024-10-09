5a883dc26b25f28f7fe7a82ad5f9858f3cd15eec
Figby222 <figby222@outlook.com>
2024-10-08 19:27:27 -0700

    Add usersController.mjs

---
+++ controllers/api/v1/usersController.mjs
@@ -0,0 +1,10 @@
+import asyncHandler from "express-async-handler";
+import db from "../../../db/queries/api/v1/usersQueries.mjs";
+
+const usersListGet = asyncHandler(async (req, res) => {
+    const users = await db.getUsersList();
+
+    res.json(users)
+})
+
+export { usersListGet };
