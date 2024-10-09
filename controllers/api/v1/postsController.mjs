8ba45da23b932d8da450d947224d0dab8ade2f6a
Figby222 <figby222@outlook.com>
2024-10-08 21:43:25 -0700

    Add postsPost() controller

--- controllers/api/v1/postsController.mjs
+++ controllers/api/v1/postsController.mjs
@@ -1,5 +1,22 @@
 import asyncHandler from "express-async-handler";
 import db from "../../../db/queries/api/v1/postQueries.mjs";
+import { body, validationResult } from "express-validator";
+
+const validatePost = [
+    body("creatorId")
+        .toInt(),
+    body("title")
+        .notEmpty().withMessage("Title must not be empty")
+        .isLength({ max: 30 }).withMessage("Title must have a maximum of 30 characters"),
+    body("text")
+        .notEmpty().withMessage("Text must not be empty")
+        .isLength({ max: 2500 }).withMessage("Text must have a maximum of 2500 characters"),
+    body("published")
+        .trim()
+        .toBoolean()
+        .notEmpty().withMessage("Published value must be selected")
+        .isBoolean().withMessage("Published value must be true or false")
+]
 
 const postsListGet = asyncHandler(async (req, res) => {
     const posts = await db.getPostsList();
@@ -7,4 +24,26 @@ const postsListGet = asyncHandler(async (req, res) => {
     res.json(posts);
 })
 
-export { postsListGet }
+const postsPost = [
+    validatePost,
+    asyncHandler(async (req, res) => {
+        const errorsResult = validationResult(req);
+        if (!errorsResult.isEmpty()) {
+            return res.json({
+                postDetails: {
+                    ...req.body,
+                },
+                errors: errorsResult.errors
+            })
+        }
+        const postDetails = await db.postPosts({
+            title: req.body.title,
+            text: req.body.text,
+            published: req.body.published,
+            creatorId: parseInt(req.body.creatorId)
+        });
+    
+        res.json(postDetails);
+    })
+]
+export { postsListGet, postsPost }
