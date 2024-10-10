438529660a19a36014167fdb489328c3bf5e95ad
Figby222 <figby222@outlook.com>
2024-10-10 02:38:31 -0700

    Add application level error handling

--- index.mjs
+++ index.mjs
@@ -9,6 +9,16 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use("/api", routers.apiRouter);
 
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    return res.status(500).json({
+        message: err.message
+    })
+})
+
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
 })
